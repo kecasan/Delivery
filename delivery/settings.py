24ee5ad044a2b87ae71c8ed76e85034da7da6745
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-05-16 14:58:00 -0300

    Update settings.py

--- delivery/settings.py
+++ delivery/settings.py
@@ -1,9 +1,6 @@
-
-
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-py)xs*8d*1&+2w^iwm$9t-mlnc91+(w@qls1lmv*(t0nw+7tn1'
@@ -11,9 +8,6 @@ SECRET_KEY = 'django-insecure-py)xs*8d*1&+2w^iwm$9t-mlnc91+(w@qls1lmv*(t0nw+7tn1
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -59,20 +53,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'delivery.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -88,10 +74,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
 
@@ -113,8 +95,5 @@ MEDIA_URL = '/media/'
 SESSION_COOKIE_AGE = 60*60*24*7
 
 SESSION_SAVE_EVERY_REQUEST = False
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
